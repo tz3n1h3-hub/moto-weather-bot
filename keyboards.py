7bb78649662dd311cb54df309c8a7e0951fdcf92
tz3n1h3-hub <tz3n1h3@gmail.com>
2026-09-13 21:58:34 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -4,26 +4,16 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_main_keyboard():
     markup = InlineKeyboardMarkup()
     markup.row(
-        InlineKeyboardButton("📊 Сейчас", callback_data="weather"),
-        InlineKeyboardButton("📅 Завтра", callback_data="forecast")
+        InlineKeyboardButton("🏍️ Сейчас", callback_data="weather"),
+        InlineKeyboardButton("ℹ️ О проекте", callback_data="about")
     )
-    markup.row(
-        InlineKeyboardButton("📆 Неделя", callback_data="weekly"),
-        InlineKeyboardButton("🏍️ Советы", callback_data="tips")
-    )
-    markup.row(InlineKeyboardButton("ℹ️ О проекте", callback_data="about"))
     return markup
 
 
 def get_after_weather_keyboard():
     markup = InlineKeyboardMarkup()
     markup.row(
-        InlineKeyboardButton("🔄 Сейчас", callback_data="update"),
-        InlineKeyboardButton("📅 Завтра", callback_data="forecast")
+        InlineKeyboardButton("🔄 Обновить", callback_data="update"),
+        InlineKeyboardButton("ℹ️ О проекте", callback_data="about")
     )
-    markup.row(
-        InlineKeyboardButton("📆 Неделя", callback_data="weekly"),
-        InlineKeyboardButton("🏍️ Советы", callback_data="tips")
-    )
-    markup.row(InlineKeyboardButton("ℹ️ О проекте", callback_data="about"))
     return markup
