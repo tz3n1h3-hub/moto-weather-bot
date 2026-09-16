5ef155e52f212f94806727d8e15373b0a927beee
tz3n1h3-hub <tz3n1h3@gmail.com>
2026-09-16 20:18:36 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -7,6 +7,9 @@ def get_main_keyboard():
         InlineKeyboardButton("🏍️ Сейчас", callback_data="weather"),
         InlineKeyboardButton("ℹ️ О проекте", callback_data="about")
     )
+    markup.row(
+        InlineKeyboardButton("🌅 Подписка на утро", callback_data="subscribe")
+    )
     return markup
 
 
@@ -22,9 +25,19 @@ def get_after_weather_keyboard():
 def get_about_keyboard():
     markup = InlineKeyboardMarkup()
     markup.row(
-        InlineKeyboardButton("🏍️ Сейчас", callback_data="weather")
+        InlineKeyboardButton("🏍️ Сейчас", callback_data="weather"),
+        InlineKeyboardButton("🌅 Подписка", callback_data="subscribe")
     )
     markup.row(
         InlineKeyboardButton("✉️ Написать разработчику", url="https://t.me/Aleksandr_K8V")
+    )
+    return markup
+
+
+def get_subscribe_keyboard():
+    markup = InlineKeyboardMarkup()
+    markup.row(
+        InlineKeyboardButton("✅ Подписаться", callback_data="subscribe_confirm"),
+        InlineKeyboardButton("❌ Отмена", callback_data="subscribe_cancel")
     )
     return markup
